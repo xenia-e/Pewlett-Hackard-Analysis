40b2419154014e25bdc4cc5e417109363f17370d
Xenia Sokol <sokol.xenia@gmail.com>
2021-07-24 21:58:29 -0400

    retirement info table added

--- schema.sql
+++ schema.sql
@@ -13,6 +13,7 @@ CREATE TABLE employees (
     gender VARCHAR NOT NULL,
     hire_date DATE NOT NULL,
     PRIMARY KEY (emp_no)
+	
 );
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
@@ -38,14 +39,14 @@ CREATE TABLE dept_emp (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
+SELECT * FROM titles;
